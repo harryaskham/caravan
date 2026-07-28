9795972a1df1f05b71fc22d03e48c2867c44188a
Cacophony Daemon <daemon@cacophony.local>
2026-07-28 08:37:51 +0100

    release: v0.0.21
    Reintegrate agent branch agent/ms-mac/caravan/ms-mac-caravan-crvn-dev-msm-0
    Agent: ms-mac-caravan-crvn-dev-msm-0
    Project: caravan
    Node: ms-mac
    Head: 0726b94
    Mode: direct
    Caco-Land: agent/ms-mac/caravan/ms-mac-caravan-crvn-dev-msm-0
    
    Comment: release: v0.0.21 version bump carrying the caravan-merge-actor auto-merge fix.

--- flake.nix
+++ flake.nix
@@ -79,7 +79,7 @@
 
         caravan = pkgs.rustPlatform.buildRustPackage {
           pname = "caravan";
-          version = "0.0.20";
+          version = "0.0.21";
           src = caravanSrc;
 
           cargoLock.lockFile = "${caravanSrc}/Cargo.lock";
