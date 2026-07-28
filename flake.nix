8386a2b9b6d1fe164a54f70e939537a6be3b9ede
Cacophony Daemon <daemon@cacophony.local>
2026-07-28 10:47:42 +0100

    release: v0.0.23
    Reintegrate agent branch agent/ms-mac/caravan/ms-mac-caravan-crvn-dev-msm-0
    Agent: ms-mac-caravan-crvn-dev-msm-0
    Project: caravan
    Node: ms-mac
    Head: f58be36
    Mode: direct
    Caco-Land: agent/ms-mac/caravan/ms-mac-caravan-crvn-dev-msm-0
    
    Comment: release: v0.0.23 carrying root flattening at creation.

--- flake.nix
+++ flake.nix
@@ -79,7 +79,7 @@
 
         caravan = pkgs.rustPlatform.buildRustPackage {
           pname = "caravan";
-          version = "0.0.22";
+          version = "0.0.23";
           src = caravanSrc;
 
           cargoLock.lockFile = "${caravanSrc}/Cargo.lock";
